0b16401c2948fe3202e16225d6216dcbfca6f103
lxyscls <lxyscls@163.com>
2017-01-08 07:30:35 +0800

    clang format

--- unbounded_spsc_queue/test.cpp
+++ unbounded_spsc_queue/test.cpp
@@ -1,17 +1,17 @@
 #include "unbounded_spsc_queue.hpp"
 
 int main(void) {
-	unbounded_spsc_queue<int> q; 
-	q.enqueue(1); 
-	q.enqueue(2); 
-	int v; 
-	bool b = q.dequeue(v); 
-	b = q.dequeue(v); 
-	q.enqueue(3); 
-	q.enqueue(4); 
-	b = q.dequeue(v); 
-	b = q.dequeue(v); 
-	b = q.dequeue(v); 
+    unbounded_spsc_queue<int> q;
+    q.enqueue(1);
+    q.enqueue(2);
+    int v;
+    bool b = q.dequeue(v);
+    b = q.dequeue(v);
+    q.enqueue(3);
+    q.enqueue(4);
+    b = q.dequeue(v);
+    b = q.dequeue(v);
+    b = q.dequeue(v);
 
-	return 0;
+    return 0;
 }
--- unbounded_spsc_queue/unbounded_spsc_queue.hpp
+++ unbounded_spsc_queue/unbounded_spsc_queue.hpp
@@ -2,86 +2,87 @@
 #include <atomic>
 
 template <typename T> T load_acquire(const T *addr) {
-  T v = *const_cast<const T volatile *>(addr);
-  std::atomic_signal_fence(std::memory_order_consume);
-  return v;
+    T v = *const_cast<const T volatile *>(addr);
+    std::atomic_signal_fence(std::memory_order_consume);
+    return v;
 }
 
 template <typename T> void store_release(T *addr, T v) {
-  std::atomic_signal_fence(std::memory_order_release);
-  *const_cast<T volatile *>(addr) = v;
+    std::atomic_signal_fence(std::memory_order_release);
+    *const_cast<T volatile *>(addr) = v;
 }
 
 size_t const cache_line_size = 64;
 
 template <typename T> class unbounded_spsc_queue {
-public:
-  unbounded_spsc_queue(void) {
-    node *n = new node();
-    head = first = tail_copy = tail = n;
-  }
-
-  ~unbounded_spsc_queue(void) {
-    node *n = first;
-    do {
-      node *_n = n->next;
-      delete n;
-      n = _n;
-    } while (n);
-  }
-
-  void enqueue(const T value) {
-    node *n = new_node();
-    n->value = value;
-    store_release(&head->next, n);
-    head = n;
-  }
-
-  bool dequeue(T &value) {
-    if (load_acquire(&tail->next)) {
-      value = tail->next->value;
-      store_release(&tail, tail->next);
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-private:
-  struct node {
-    node *next;
-    T value;
-  };
-
-  node *new_node() {
-    if (first != tail_copy) {
-      node *n = first;
-      first = first->next;
-      return n;
+  public:
+    unbounded_spsc_queue(void) {
+        node *n = new node();
+        head = first = tail_copy = tail = n;
     }
 
-    tail_copy = load_acquire(&tail);
-    if (first != tail_copy) {
-      node *n = first;
-      first = first->next;
-      return n;
+    ~unbounded_spsc_queue(void) {
+        node *n = first;
+        do {
+            node *_n = n->next;
+            delete n;
+            n = _n;
+        } while (n);
     }
 
-    node *n = new node();
-    return n;
-  }
+    void enqueue(const T &data) {
+        node *n = alloc_node();
+        n->data = data;
+        n->next = nullptr;
+        store_release(&head->next, n);
+        head = n;
+    }
 
-  // consumer
-  node *tail;
+    bool dequeue(T &data) {
+        if (load_acquire(&tail->next)) {
+            data = tail->next->data;
+            store_release(&tail, tail->next);
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-  // padding forbid "False sharing"
-  char padding[cache_line_size];
+  private:
+    struct node {
+        node *next;
+        T data;
+    };
 
-  // producer
-  node *head;
-  node *first;
-  node *tail_copy;
+    node *alloc_node() {
+        if (first != tail_copy) {
+            node *n = first;
+            first = first->next;
+            return n;
+        }
 
-  unbounded_spsc_queue(const unbounded_spsc_queue &);
-  unbounded_spsc_queue &operator=(const unbounded_spsc_queue &);
+        tail_copy = load_acquire(&tail);
+        if (first != tail_copy) {
+            node *n = first;
+            first = first->next;
+            return n;
+        }
+
+        node *n = new node();
+        return n;
+    }
+
+    // consumer
+    node *tail;
+
+    // padding forbid "False sharing"
+    char padding[cache_line_size];
+
+    // producer
+    node *head;
+    node *first;
+    node *tail_copy;
+
+    unbounded_spsc_queue(const unbounded_spsc_queue &);
+    unbounded_spsc_queue &operator=(const unbounded_spsc_queue &);
 };
