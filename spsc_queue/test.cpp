4195884ef8703cc6f763601fc2ad1668d3b05a55
lxyscls <lxyscls@163.com>
2017-01-18 06:15:55 +0800

    test and bench

---
+++ spsc_queue/test.cpp
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2016 Erik Rigtorp <erik@rigtorp.se>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included in
+ * all
+ * copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include "spsc_queue.hpp"
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <atomic>
+#include <cassert>
+
+int main(void) {
+    const size_t iter = 100000;
+    spsc_queue<size_t, 512> q(-1);
+    std::atomic<bool> flag(false);
+    std::thread producer([&] {
+        while (!flag)
+            ;
+        for (size_t i = 0; i < iter; ++i) {
+            if (!q.enqueue(i))
+				--i;
+        }
+    });
+
+    size_t sum = 0;
+    auto start = std::chrono::system_clock::now();
+    flag = true;
+    for (size_t v, i = 0; i < iter; ++i) {
+        if (q.dequeue(v))
+            sum += v;
+        else
+            --i;
+    }
+    auto end = std::chrono::system_clock::now();
+    auto duration =
+        std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
+
+    assert(sum == iter * (iter - 1) / 2);
+
+    producer.join();
+
+    std::cout << duration.count() / iter << " ns/iter" << std::endl;
+
+    return 0;
+}
